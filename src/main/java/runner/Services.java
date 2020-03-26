49c610f0450106604df0c36049a9a8771694ff18
SergeyShidlovsky <shedlovskys@ukr.net>
2020-03-27 00:01:02 +0200

    Added all buttons to Services tab

--- src/main/java/runner/Services.java
+++ src/main/java/runner/Services.java
@@ -16,13 +16,40 @@ public class Services extends JPanel {
 
     Services(final Timer timer, final TimerTick tm, Font font, JLabel[] statusLabel) {
 
-        final JButton button1 = new JButton("GetInstallLog [1]");
-
+        final JButton button1 = new JButton("Open Services [1]");
         button1.setFont(font);
         button1.setVisible(true);
-        button1.setToolTipText("Copy setuoapi.dev.log to Desktop");
+        button1.setToolTipText("Open Services window");
         button1.setSize(313, 110);
         add(button1);
+
+        final JButton button2 = new JButton("Start Audio Service [2]");
+        button2.setFont(font);
+        button2.setVisible(true);
+        button2.setToolTipText("Start Audio Service.");
+        button2.setSize(313, 110);
+        add(button2);
+
+        final JButton button3 = new JButton("Start Integration Service [3]");
+        button3.setFont(font);
+        button3.setVisible(true);
+        button3.setToolTipText("Start Integration Service");
+        button3.setSize(313, 110);
+        add(button3);
+
+        final JButton button4 = new JButton("Stop Audio Service [4]");
+        button4.setFont(font);
+        button4.setVisible(true);
+        button4.setToolTipText("Stop Audio Service");
+        button4.setSize(313, 110);
+        add(button4);
+
+        final JButton button5 = new JButton("Stop Integration Service [5]");
+        button5.setFont(font);
+        button5.setVisible(true);
+        button5.setToolTipText("Stop Integration Service");
+        button5.setSize(313, 110);
+        add(button5);
 
         //Add ActionListeners of Button1
         button1.addActionListener(new ActionListener() {
@@ -30,8 +57,68 @@ public class Services extends JPanel {
                 //Button1 will perform next actions
                 if (ae.getSource() == button1) {
                     try {
-                        Runtime.getRuntime().exec("C:\\toolForRunners\\Installation\\GetInstallLog.bat");
+                        Runtime.getRuntime().exec("C:\\toolForRunners\\Services\\OpenServices.bat");
                         timeReset(2, timer, tm); //Set time of Script Execution Here
+                    } catch (Exception r) {
+                        tm.showException();
+                    }
+                }
+            }
+        });
+
+        //Add ActionListeners of Button2
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                //Button2 will perform next actions
+                if (ae.getSource() == button2) {
+                    try {
+                        Runtime.getRuntime().exec("C:\\toolForRunners\\Services\\StartAudioService.bat");
+                        timeReset(3, timer, tm); //Set time of Script Execution Here
+                    } catch (Exception r) {
+                        tm.showException();
+                    }
+                }
+            }
+        });
+
+        //Add ActionListeners of Button3
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                //Button2 will perform next actions
+                if (ae.getSource() == button3) {
+                    try {
+                        Runtime.getRuntime().exec("C:\\toolForRunners\\Services\\StartIntegrationService.bat");
+                        timeReset(4, timer, tm); //Set time of Script Execution Here
+                    } catch (Exception r) {
+                        tm.showException();
+                    }
+                }
+            }
+        });
+
+        //Add ActionListeners of Button4
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                //Button2 will perform next actions
+                if (ae.getSource() == button4) {
+                    try {
+                        Runtime.getRuntime().exec("C:\\toolForRunners\\Services\\StopAudioService.bat");
+                        timeReset(3, timer, tm); //Set time of Script Execution Here
+                    } catch (Exception r) {
+                        tm.showException();
+                    }
+                }
+            }
+        });
+
+        //Add ActionListeners of Button5
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                //Button2 will perform next actions
+                if (ae.getSource() == button5) {
+                    try {
+                        Runtime.getRuntime().exec("C:\\toolForRunners\\Services\\StopIntegrationService.bat");
+                        timeReset(5, timer, tm); //Set time of Script Execution Here
                     } catch (Exception r) {
                         tm.showException();
                     }
@@ -47,15 +134,27 @@ public class Services extends JPanel {
                     case KeyEvent.VK_1:
                         button1.doClick();
                         break;
-
-                    //case KeyEvent.VK_2:
-                    //    button2.doClick();
-                    //    break;
+                    case KeyEvent.VK_2:
+                        button2.doClick();
+                        break;
+                    case KeyEvent.VK_3:
+                        button3.doClick();
+                        break;
+                    case KeyEvent.VK_4:
+                        button4.doClick();
+                        break;
+                    case KeyEvent.VK_5:
+                        button5.doClick();
+                        break;
                 }
             }
         };
 
         button1.addKeyListener(listener8);
+        button2.addKeyListener(listener8);
+        button3.addKeyListener(listener8);
+        button4.addKeyListener(listener8);
+        button5.addKeyListener(listener8);
         add(statusLabel[8]);
         this.addKeyListener(listener8);
     }
