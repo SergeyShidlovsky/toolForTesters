bc5805f811951219f5c4ea1156400196f317a779
SergeyShidlovsky <shedlovskys@ukr.net>
2020-04-02 17:40:32 +0300

    Implemented all buttons in Execute tab

--- src/main/java/runner/tabs/Execute.java
+++ src/main/java/runner/tabs/Execute.java
@@ -19,33 +19,61 @@ public class Execute extends JPanel  {
 
     public Execute(final Timer timer, final TimerTick tm, Font font, JLabel[] statusLabel) {
 
-        final JButton button1 = new JButton("Device and Printers");
+        final JButton button1 = new JButton("Application wizard [1]");
         button1.setFont(font);
         button1.setVisible(true);
-        button1.setToolTipText("Open Device and Printers");
+        button1.setToolTipText("Open Application wizard");
         button1.setSize(313, 110);
         add(button1);
 
-        final JButton button2 = new JButton("Device Manager");
+        final JButton button2 = new JButton("Audio Settings [2}");
         button2.setFont(font);
         button2.setVisible(true);
-        button2.setToolTipText("Open Device manger");
+        button2.setToolTipText("Open Audio Settings r");
         button2.setSize(313, 110);
         add(button2);
 
-        final JButton button3 = new JButton(" Application wizard ");
+        final JButton button3 = new JButton(" Device Manager [3] ");
         button3.setFont(font);
         button3.setVisible(true);
-        button3.setToolTipText("Open Application wizard");
+        button3.setToolTipText("Open Device Manager");
         button3.setSize(313, 110);
         add(button3);
 
-        final JButton button4 = new JButton(" Registry Editor");
+        final JButton button4 = new JButton(" Device and Printers [4]");
         button4.setFont(font);
         button4.setVisible(true);
-        button4.setToolTipText("Open Registry Editor");
+        button4.setToolTipText("Open Device and Printers");
         button4.setSize(313, 110);
         add(button4);
+
+        final JButton button5 = new JButton(" Device Metadata [5]");
+        button5.setFont(font);
+        button5.setVisible(true);
+        button5.setToolTipText("Open Device Metadata");
+        button5.setSize(313, 110);
+        add(button5);
+
+        final JButton button6 = new JButton(" Remote Desktop [6]");
+        button6.setFont(font);
+        button6.setVisible(true);
+        button6.setToolTipText("Open RDP Connections");
+        button6.setSize(313, 110);
+        add(button6);
+
+        final JButton button7 = new JButton(" Task Manager [7]");
+        button7.setFont(font);
+        button7.setVisible(true);
+        button7.setToolTipText("Open Device Manager");
+        button7.setSize(313, 110);
+        add(button7);
+
+        final JButton button8 = new JButton(" Update Server [8]");
+        button8.setFont(font);
+        button8.setVisible(true);
+        button8.setToolTipText("Open Update server");
+        button8.setSize(313, 110);
+        add(button8);
 
         button1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent ae) {
@@ -103,6 +131,62 @@ public class Execute extends JPanel  {
             }
         });
 
+        button5.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                //Button4 will perform next actions
+                if (ae.getSource() == button5) {
+                    try {
+                        Runtime.getRuntime().exec("C:\\toolForRunners\\Exec\\");
+                        timeReset(2, timer, tm); //Set time of Script Execution Here
+                    } catch (Exception r) {
+                        tm.showException();
+                    }
+                }
+            }
+        });
+
+        button6.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                //Button4 will perform next actions
+                if (ae.getSource() == button6) {
+                    try {
+                        Runtime.getRuntime().exec("C:\\toolForRunners\\Exec\\");
+                        timeReset(2, timer, tm); //Set time of Script Execution Here
+                    } catch (Exception r) {
+                        tm.showException();
+                    }
+                }
+            }
+        });
+
+        button7.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                //Button4 will perform next actions
+                if (ae.getSource() == button7) {
+                    try {
+                        Runtime.getRuntime().exec("C:\\toolForRunners\\Exec\\");
+                        timeReset(2, timer, tm); //Set time of Script Execution Here
+                    } catch (Exception r) {
+                        tm.showException();
+                    }
+                }
+            }
+        });
+
+        button8.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+                //Button4 will perform next actions
+                if (ae.getSource() == button8) {
+                    try {
+                        Runtime.getRuntime().exec("C:\\toolForRunners\\Exec\\");
+                        timeReset(2, timer, tm); //Set time of Script Execution Here
+                    } catch (Exception r) {
+                        tm.showException();
+                    }
+                }
+            }
+        });
+
         //Add KeyListener for Button1 press emulation by pressing Num1 button
         KeyListener listener3 = new KeyAdapter() {
             @Override
@@ -120,11 +204,30 @@ public class Execute extends JPanel  {
                     case KeyEvent.VK_4:
                         button4.doClick();
                         break;
+                    case KeyEvent.VK_5:
+                        button5.doClick();
+                        break;
+                    case KeyEvent.VK_6:
+                        button6.doClick();
+                        break;
+                    case KeyEvent.VK_7:
+                        button7.doClick();
+                        break;
+                    case KeyEvent.VK_8:
+                        button8.doClick();
+                        break;
                 }
             }
         };
 
         button1.addKeyListener(listener3);
+        button2.addKeyListener(listener3);
+        button3.addKeyListener(listener3);
+        button4.addKeyListener(listener3);
+        button5.addKeyListener(listener3);
+        button6.addKeyListener(listener3);
+        button7.addKeyListener(listener3);
+        button8.addKeyListener(listener3);
         add(statusLabel[3]);
         this.addKeyListener(listener3);
     }
