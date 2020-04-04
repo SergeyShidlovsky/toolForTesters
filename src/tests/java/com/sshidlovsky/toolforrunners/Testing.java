0739216656b3345f4c0fc7c5187b324056588d47
SergeyShidlovsky <shedlovskys@ukr.net>
2020-04-04 21:16:54 +0300

    Implemented maven project structure

--- src/tests/java/com/sshidlovsky/toolforrunners/Testing.java
+++ src/tests/java/com/sshidlovsky/toolforrunners/Testing.java
@@ -1,4 +1,4 @@
-package java.com.sshidlovsky.toolforrunners;
+package com.sshidlovsky.toolforrunners;
 
 import com.sshidlovsky.toolforrunners.runner.TimerTick;
 import static org.junit.Assert.*;
@@ -10,26 +10,32 @@ public class Testing {
 
     @Test
     public void testShowTimerException() {
-        TimerTick tm = new TimerTick();
 
-        JLabel labelOne = new JLabel();
-        JLabel labelTwo = new JLabel();
-        JLabel labelThree = new JLabel();
-
-        labelOne.setText("OriginalNameOne");
-        labelTwo.setText("OriginalNameTwo");
-        labelThree.setText("OriginalNameThree");
-
-        JLabel statusLabel[] = new JLabel[2];
-
-        statusLabel[0] = labelOne;
-        statusLabel[1] = labelTwo;
-        statusLabel[2] = labelThree;
-
-        tm.showException();
-        assertEquals("Wrong exception caption",
-                     "                   Script starting failed. Try again                  ",
-                    statusLabel[0].getText());
-
+        //Todo Please complete implementation of the first unit test(Uncomment it) and add two more assertions
+//        final TimerTick tm = new TimerTick();
+//        final Timer timer = new Timer(1000, tm);
+//        tm.setTimer(timer);;
+//
+//
+//        JLabel labelOne = new JLabel();
+//        JLabel labelTwo = new JLabel();
+//        JLabel labelThree = new JLabel();
+//
+//        labelOne.setText("OriginalNameOne");
+//        labelTwo.setText("OriginalNameTwo");
+//        labelThree.setText("OriginalNameThree");
+//
+//        JLabel statusLabel[] = new JLabel[3];
+//
+//        statusLabel[0] = labelOne;
+//        statusLabel[1] = labelTwo;
+//        statusLabel[2] = labelThree;
+//
+//        tm.setStatusLabel(statusLabel);
+//        timer.start();
+//        tm.showException();
+//        assertEquals("Wrong exception caption",
+//                     "                   Script starting failed. Try again                  ",
+//                    statusLabel[0].getText());
     }
 }
