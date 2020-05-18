cd552a8df0c3d6b5b87a72035725640ab534fbc2
SergeyShidlovsky <shedlovskys@ukr.net>
2020-05-18 10:33:52 +0300

    Move-buttons-creaation to separaste method in Services tab

--- src/main/java/com/sshidlovsky/toolforrunners/tabs/Services.java
+++ src/main/java/com/sshidlovsky/toolforrunners/tabs/Services.java
@@ -18,9 +18,9 @@ public class Services extends JPanel {
     //Adding method for reset timer with new value
     // after buttons below have been pressed
     public void timeReset(int seconds, Timer timer, TimerTick tm) {
-        timer.stop();             //Stopping previous timer before execution of current script
-        tm.setCountdown(seconds); //Setting time of  Script execution
-        timer.start();            //Starting timer after script initiation
+        timer.stop();
+        tm.setCountdown(seconds);
+        timer.start();
     }
 
     private void addActionListenerToButton(final JButton button, final String command, final Timer timer,
