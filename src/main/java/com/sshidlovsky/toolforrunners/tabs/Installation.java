8451ffa598d8832fd93bcc0549bf44253e361b10
SergeyShidlovsky <shedlovskys@ukr.net>
2020-07-13 23:06:23 +0300

    Moved buttons creation to separate method in Installation tab

--- src/main/java/com/sshidlovsky/toolforrunners/tabs/Installation.java
+++ src/main/java/com/sshidlovsky/toolforrunners/tabs/Installation.java
@@ -9,6 +9,9 @@ import java.awt.event.*;
 
 public class Installation extends JPanel {
 
+    private JButton button1;
+    private JButton button2;
+
     //Adding method for reset timer with new value
     // after buttons below have been pressed
     //todo Move this method to abstract class
@@ -35,21 +38,23 @@ public class Installation extends JPanel {
         });
     }
 
+    private void addButtonWithPreferencesToTab(final JButton button, String tooltip, Font font) {
+        button.setFont(font);
+        button.setVisible(true);
+        button.setToolTipText(tooltip);
+        button.setSize(313, 110);
+        add(button);
+    }
+
     public Installation(final Timer timer, final TimerTick tm, Font font, JLabel[] statusLabel) {
 
-        final JButton button1 = new JButton("GetInstallLog [1]");
-        button1.setFont(font);
-        button1.setVisible(true);
-        button1.setToolTipText("Copy setuoapi.dev.log to Desktop");
-        button1.setSize(313, 110);
-        add(button1);
+        //Create list of buttons
+        button1 = new JButton("GetInstallLog [1]");
+        button2 = new JButton("GetOEMFiles [2]");
 
-        final JButton button2 = new JButton("GetOEMFiles [2]");
-        button2.setFont(font);
-        button2.setVisible(true);
-        button2.setToolTipText("Copy OEM files to Desktop");
-        button2.setSize(313, 110);
-        add(button2);
+        //Add all buttons to tab
+        addButtonWithPreferencesToTab(button1, "Copy setuoapi.dev.log to Desktop", font);
+        addButtonWithPreferencesToTab(button2, "Copy OEM files to Desktop", font);
 
         //Add ActionListeners on all buttons
         addActionListenerToButton(button1, LinksInstallation.GET_OEM_FILES.getValue(), timer, tm, 2);
