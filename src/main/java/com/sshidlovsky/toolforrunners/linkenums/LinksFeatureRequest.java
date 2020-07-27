66a1b23abebc0f65a0908c1efd63bc2da8309670
SergeyShidlovsky <shedlovskys@ukr.net>
2020-07-27 03:35:55 +0300

    Move common code in FeatureRequestOps class to private methods

--- src/main/java/com/sshidlovsky/toolforrunners/linkenums/LinksFeatureRequest.java
+++ src/main/java/com/sshidlovsky/toolforrunners/linkenums/LinksFeatureRequest.java
@@ -1,5 +1,8 @@
 package com.sshidlovsky.toolforrunners.linkenums;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public enum LinksFeatureRequest {
 
     //ToDo https://github.com/SergeyShidlovsky/toolForTesters/issues/76
@@ -8,7 +11,8 @@ public enum LinksFeatureRequest {
     ADD_ISSUE_TO_BOARD_URL("/projects/columns/8414334/cards"),
     ACCESS_TOKEN("d627d46bef57e03be53033e4e16d4341d25194ad"),
     TITLE("Assign an issue on board"),
-    DESCRIPTION("Created by tooolforrunners And Assigned to a board"),
+    DESCRIPTION("Created by tooolforrunners And Assigned to a board in " +
+            Calendar.getInstance().getTimeInMillis()),
     LABEL("Bug"),
     CONTENT_TYPE("Issue");
 
